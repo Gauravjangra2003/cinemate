906017721f9a9067aec145e996e8e3750cb68b60
GAURAV KUMAR <gk813589@gmail.com>
2025-03-21 23:34:00 +0530

    UI Design

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,5 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-  return <div>This is Footer</div>;
+  return (
+    <footer>
+      <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2030{" "}
+            <Link to="/" className="hover:underline">
+              Cinemate
+            </Link>
+            . All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <Link
+                href="#"
+                target="_blank"
+                className="hover:underline me-4 md:me-6"
+              >
+                Instagram
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                target="_blank"
+                className="hover:underline me-4 md:me-6"
+              >
+                Youtube
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                target="_blank"
+                className="hover:underline me-4 md:me-6"
+              >
+                LinkedIn
+              </Link>
+            </li>
+            <li>
+              <Link href="#" target="_blank" className="hover:underline">
+                Github
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </footer>
+  );
 };
