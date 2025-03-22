870f840fa00b9a8e6581d52610c397d65307050e
GAURAV KUMAR <gk813589@gmail.com>
2025-03-22 13:21:51 +0530

    api fetch of search bar

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { Card } from "../component/Card";
+import useFetch from "../hooks/useFetch";
 
-export const Search = () => {
-  return <main>Search</main>;
+export const Search = ({ apiPath }) => {
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies } = useFetch(apiPath, queryTerm);
+  return (
+    <main>
+      <section>
+        <p className="text-3xl text-gray-700 dark:text-white">
+          {movies.length === 0
+            ? `No result found for '${queryTerm}'`
+            : `Result for '${queryTerm}'`}
+        </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
