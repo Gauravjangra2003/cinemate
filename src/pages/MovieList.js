407ecce2930abe8a3c31b672b6cb955518ef1f73
GAURAV KUMAR <gk813589@gmail.com>
2025-03-22 17:51:08 +0530

    commit

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,8 +3,8 @@ import useFetch from "../hooks/useFetch";
 import useTitle from "../hooks/useTitle";
 export const MovieList = ({ apiPath, title }) => {
   const { data: movies } = useFetch(apiPath);
-
-  const pageTitle = useTitle(title);
+  // const pageTitle = useTitle(title);
+  useTitle(title);
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
