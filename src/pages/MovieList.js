696ae75cd560d2188a63a65aef3fe2117c0f784b
GAURAV KUMAR <gk813589@gmail.com>
2025-03-22 12:39:54 +0530

    custom use fetch

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,18 +1,7 @@
-import React, { useEffect, useState } from "react";
 import { Card } from "../component/Card";
-export const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=b80d59c33d6d57ed9c7e3713f91c188a"
-      );
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+import useFetch from "../hooks/useFetch";
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
