d4acd3adbe23a7933252932c6ba252af7de637b2
GAURAV KUMAR <gk813589@gmail.com>
2025-03-22 17:05:12 +0530

    giving scroll

--- src/component/index.js
+++ src/component/index.js
@@ -1,3 +1,6 @@
 export { Header } from "../component/Header";
 export { Footer } from "../component/Footer";
+
 export { Card } from "../component/Card";
+
+export { ScrollToTop } from "../component/ScrollToTop";
